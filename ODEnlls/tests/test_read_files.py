13a85ba6fa19ede9b9439422a53f2d8e5e4297d3
Ryan Nelson <rnelsonchem@gmail.com>
2017-12-24 17:34:58 -0500

    Added read_data/_function_string test
    As a surrogate for testing the file reading function, I've decided to
    check the full function string that gets compiled from a test case.
    There are other variables that need to be tested after running this
    function though.

--- ODEnlls/tests/test_read_files.py
+++ ODEnlls/tests/test_read_files.py
@@ -15,3 +15,18 @@ def test_read_data():
     answer = pd.read_pickle(os.path.join(current_directory, 
                                         'sample_data.pkl'))
     assert np.alltrue(answer == x.data)
+
+def test_function_string():
+    '''This test is surrogate for testing the read_rxns method. There are
+    other things to be tested in this function as well.
+    '''
+    x = ODEnlls()
+    x.read_rxns(os.path.join(current_directory, 'sample_rxns.txt'))
+    answer = """def f(y, t, k1, k2, k3, k4, k5, k6):
+    return [
+        1.00*(-1*k1*(y[0]) + k2*(y[1])) + 1.00*(-1*k3*(y[0]) + k4*(y[2])) + 1.00*(-1*k5*(y[0]) + k6*(y[3])),
+        1.00*(k1*(y[0]) + -1*k2*(y[1])),
+        1.00*(k3*(y[0]) + -1*k4*(y[2])),
+        1.00*(k5*(y[0]) + -1*k6*(y[3])),
+        ]"""
+    assert x._function_string == answer
