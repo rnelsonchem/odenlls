c0a248083d5f6fe8ed646c62581d57a9f3546cd7
Ryan Nelson <rnelsonchem@gmail.com>
2017-12-24 16:33:34 -0500

    Added individual rxn parsing tests
    These tests will check the output of _rxnRate method.

---
+++ ODEnlls/tests/test_individual_reaction_parse.py
@@ -0,0 +1,38 @@
+import pytest
+
+from ..ODEnlls import ODEnlls
+
+def test_irrev_A_to_B():
+    x = ODEnlls()
+    rxn = x._rxnRate('A -> B')
+    answer = (1, ['A', 'B'], ['1.00*(-1*k1*(A))', '1.00*(k1*(A))'])
+    assert rxn == answer
+
+def test_irrev_A_to_B_C():
+    x = ODEnlls()
+    rxn = x._rxnRate('A -> B + C')
+    answer = (1, ['A', 'B', 'C'], 
+            ['1.00*(-1*k1*(A))', '1.00*(k1*(A))', '1.00*(k1*(A))'])
+    assert rxn == answer
+
+def test_irrev_A_B_to_C():
+    x = ODEnlls()
+    rxn = x._rxnRate('A + B -> C')
+    answer = (1, ['A', 'B', 'C'],
+        ['1.00*(-1*k1*(A)*(B))', '1.00*(-1*k1*(A)*(B))', 
+            '1.00*(k1*(A)*(B))']
+        )
+    assert rxn == answer
+
+def test_irrev_A_to_2B():
+    x = ODEnlls()
+    rxn = x._rxnRate('A -> 2*B')
+    answer = (1, ['A', 'B'], ['1.00*(-1*k1*(A))', '2.00*(k1*(A))'])
+    assert rxn == answer
+
+def test_irrev_2A_to_B():
+    x = ODEnlls()
+    rxn = x._rxnRate('2*A -> B')
+    answer = (1, ['A', 'B'],
+        ['2.00*(-1*k1*((A**2.00)/2.00))', '1.00*(k1*((A**2.00)/2.00))'])
+    assert rxn == answer
