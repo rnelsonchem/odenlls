Fix Chi squared calculation
The old chi-squared calc was not dividing the squared deviations by the
original values. I this affected the calculation of a couple of other
values, so I created a new sum-of-squares parameter as well to replace
the original chi squared variable.

ODEnlls/ODEnlls.py
@@ -364,13 +364,15 @@ class ODEnlls():
         self.residuals = pd.concat([times, res_temp], axis=1)
 
         # Create some statistical parameters for the fit
-        # Chi squared
-        chisq = (info["fvec"]**2).sum()
+        # Chi squared and sum of squares (ssq)
+        squared = info["fvec"]**2
+        ssq = squared.sum()
+        chisq = (squared/raw.flatten()).sum()
 
         # R-squared
         y_ave = raw.mean()
         yave_diff = ((raw - y_ave)**2).sum()
-        rsq = 1.0 - (chisq/yave_diff)
+        rsq = 1.0 - (ssq/yave_diff)
 
         # Akaike's Information Criterion. The use of this parameter is based
         # on the following reference, which is specific to chemical kinetics
@@ -379,13 +381,13 @@ class ODEnlls():
         # chemical kinetics.
         lenp = len(p)
         lenfvec = len(info["fvec"])
-        aic = lenfvec*np.log(chisq) + 2*lenp + \
+        aic = lenfvec*np.log(ssq) + 2*lenp + \
                 ( 2*lenp*(lenp + 1) )/(lenfvec - lenp - 1)
 
         # Calculate parameter errors. Make sure that parameter errors don't
         # get set for fixed values
         dof = lenfvec - lenp
-        sqcsdof = np.sqrt(chisq/dof)
+        sqcsdof = np.sqrt(ssq/dof)
         sigma = np.sqrt(np.diag(cov))*sqcsdof
         self.params['error'] = np.nan
         self.params.loc[mask, 'error'] = sigma